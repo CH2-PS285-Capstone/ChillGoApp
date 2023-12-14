071dfb9a4318d52dbc73958ec5069119e2336b50
diks15 <125732993+diks15@users.noreply.github.com>
2023-12-14 22:07:05 +0700

    Delete src directory

--- src/routes/loginRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const loginController = require('../controllers/LoginController');
-
-// Rute untuk melakukan login
-router.post('/login', loginController.login);
-
-module.exports = router;
