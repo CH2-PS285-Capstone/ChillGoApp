392e390a4c305f2a6f162dad7dfe2208f4187866
lshfra <145365560+lshfra@users.noreply.github.com>
2023-12-15 06:18:37 +0700

    Update gcs.config.js

--- src/config/gcs.config.js
+++ src/config/gcs.config.js
@@ -2,7 +2,7 @@
 const { Storage } = require('@google-cloud/storage');
 
 const storage = new Storage({
-    projectId: 'capstone-project-405714', // Ganti dengan ID proyek Google Cloud Anda
+    projectId: '-', // Ganti dengan ID proyek Google Cloud Anda
     keyFilename: 'capstone-project-405714-1404ad62089b.json', // Ganti dengan path menuju berkas kunci layanan GCS Anda
 });
 
