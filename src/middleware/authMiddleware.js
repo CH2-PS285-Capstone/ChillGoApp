f54a8d2e6f62d8f45749e3a44895d28300352f35
lshfra <145365560+lshfra@users.noreply.github.com>
2023-12-16 18:11:31 +0700

    Delete src directory

--- src/middleware/authMiddleware.js
+++
@@ -1,9 +0,0 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
-
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: 'https://your-project-id.firebaseio.com' // Ganti dengan URL database Firebase Anda
-});
-
-module.exports = admin;
